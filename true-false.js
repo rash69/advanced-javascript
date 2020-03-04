09a87d81a09f18668be06b45060b735e40798a79
rash69 <imbusy12340@gmail.com>
2020-03-04 21:46:06 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,18 @@
+//Falsey
+//false
+// 0
+// "";
+// null
+// NaN
+
+//Truthy
+//'0' ' '
+
+let name = 12;
+console.log(name);
+if(name || name == 0){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is false");
+}
