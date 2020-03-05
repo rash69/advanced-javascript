3e3b9933c7d3e4d53e8d3a18d8ef0145a45e10f4
rash69 <imbusy12340@gmail.com>
2020-03-05 16:28:12 +0600

    onk hoise

---
+++ array-split.js
@@ -0,0 +1,10 @@
+const numbs = [1, 2, 3, 4, 5, 6, 7, 8];
+//const part = numbs.slice(2, 5);
+
+const removed = numbs.splice(2, 3, 77, 88, 99, 101);
+
+// console.log(removed);
+// console.log(numbs);
+
+const together = numbs.join("")
+console.log(together);
